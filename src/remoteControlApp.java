f1117b09d97aea91064e490444cd876196f1717f
glabrie10 <gabriel.labrie.professinal@gmail.com>
2019-11-08 16:44:50 -0700

    New TODO

--- src/remoteControlApp.java
+++ src/remoteControlApp.java
@@ -2,9 +2,9 @@
 public class remoteControlApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 // TODO add press button method
 // TODO add Turn on method
+	
 }
