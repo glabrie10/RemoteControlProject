6641eedb42a6ea6f13ca95596b0822053b6d7323
glabrie10 <gabriel.labrie.professinal@gmail.com>
2019-11-09 13:28:17 -0700

    Commit

--- src/remoteControlApp.java
+++ src/remoteControlApp.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 
 public class remoteControlApp {
 
 	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.In);
+		
+		System.out.println("Press On to turn on the remote:");
+		
+		String ON = keyboard.next();
+		
+		while ()
+		
 		
 	}
 // TODO add press button method
