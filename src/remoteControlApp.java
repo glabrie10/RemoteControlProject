d4477e3d271d1286c462af9ef7ddfd0c638e3d84
glabrie10 <gabriel.labrie.professinal@gmail.com>
2019-11-13 18:52:11 -0700

    added loops for enter message/fixed loop for switch

--- src/remoteControlApp.java
+++ src/remoteControlApp.java
@@ -1,4 +1,3 @@
-package Labs;
 
 import java.util.Scanner;
 
@@ -7,69 +6,81 @@ public class remoteControlApp {
 	public static void main(String[] args) {
 
 		Scanner Keyboard = new Scanner(System.in);
-		System.out.println("To begin turn on the remote control by typing On");
-		String choice = Keyboard.next().toLowerCase();
+		
+		boolean on = true;
+		while (on == true) {
+			System.out.println("To begin turn on the remote control by typing On");
+			String choice = Keyboard.next().toLowerCase();
+			
+			if (choice.equals("on")) {
+				on = false;
+				pressButton();
+			}
 
-		while (!choice.equals("on")) {
-			choice = Keyboard.next().toLowerCase();
-			//Runs until true
+			// Runs until true
 		}
-		pressButton();
+
 	}
 
 	public static void pressButton() {
+
 		Scanner Keyboard = new Scanner(System.in);
 		System.out.println("Turning On");
 		System.out.println("BOOP");
-		System.out.println("Select a number 0 through 9, or Off to exit remote.");
-		String choice = Keyboard.next();
-		switch (choice) {
-			case "0":
-				System.out.println("Beep " + 0 + " choose again or push Off to exit remote");
-				break;
-			case "1":
-				System.out.println("Beep " + 1 + " choose again or push Off to exit remote");
-				break;
-			case "2":
-				System.out.println("Beep " + 2 + " choose again or push Off to exit remote");
-				break;
-			case "3": 
-				System.out.println("Beep " + 3 + " choose again or push Off to exit remote");
-				break;
-			case "4":
-				System.out.println("Beep " + 4 + " choose again or push Off to exit remote");
-				break;
-			case "5":
-				System.out.println("Beep " + 5 + " choose again or push Off to exit remote");
-				break;
-			case "6":
-				System.out.println("Beep " + 6 + " choose again or push Off to exit remote");
-				break;
-			case "7":
-				System.out.println("Beep " + 7 + " choose again or push Off to exit remote");
-				break;
-			case "8":
-				System.out.println("Beep " + 8 + " choose again or push Off to exit remote");
-				break;
-			case "9":
-				System.out.println("Beep " + 9 + " choose again or push Off to exit remote");
-				pressButton();
+		boolean stayOn = true;
+		while (stayOn == true) {
 
-				break;
-			case "off":
-				remoteOff();
-				break;
-				
+			System.out.println("Select a number 0 through 9, or Off to exit remote.");
+			String choice = Keyboard.next();
+			switch (choice) {
+				case "0":
+					System.out.println("Beep " + 0 + " choose again or push Off to exit remote");
+					break;
+				case "1":
+					System.out.println("Beep " + 1 + " choose again or push Off to exit remote");
+					break;
+				case "2":
+					System.out.println("Beep " + 2 + " choose again or push Off to exit remote");
+					break;
+				case "3":
+					System.out.println("Beep " + 3 + " choose again or push Off to exit remote");
+					break;
+				case "4":
+					System.out.println("Beep " + 4 + " choose again or push Off to exit remote");
+					break;
+				case "5":
+					System.out.println("Beep " + 5 + " choose again or push Off to exit remote");
+					break;
+				case "6":
+					System.out.println("Beep " + 6 + " choose again or push Off to exit remote");
+					break;
+				case "7":
+					System.out.println("Beep " + 7 + " choose again or push Off to exit remote");
+					break;
+				case "8":
+					System.out.println("Beep " + 8 + " choose again or push Off to exit remote");
+					break;
+				case "9":
+					System.out.println("Beep " + 9 + " choose again or push Off to exit remote");
+					break;
+
 				default:
-				System.out.println("Error choose a number 1 - 0 or Off to exit remote");
-				pressButton();
-				
+					System.out.println("Error choose a number 1 - 0 or Off to exit remote");
+					pressButton();
+					break;
+				case "off":
+					stayOn = false;
+					remoteOff();
+
+					Keyboard.close();
+					
+			}
+
 		}
-		Keyboard.close();
+
 	}
 
 	public static void remoteOff() {
 		System.out.println("Turning Off");
 	}
 }
-	
